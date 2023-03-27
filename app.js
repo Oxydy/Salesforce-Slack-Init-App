0fc555bb8202a8bab910760f8c75047a6b91bcba
oxydy <dwhongwei@gmail.com>
2023-03-28 00:33:08 +0800

    Commit

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const { App } = require('@slack/bolt');
 
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    port: process.env.PORT || 3000
+    token: process.env.SLACK_APP_BOT_TOKEN,
+    signingSecret: process.env.SLACK_APP_SIGNING_SECRET
 });
 
 (async () => {
-    await app.start();
+    const port = 3000
+    await app.start(process.env.PORT || port);
     console.log('⚡️ Bolt app is running');
 })();
