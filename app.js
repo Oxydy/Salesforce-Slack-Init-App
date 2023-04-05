109c82290465082a509c33409ee28850d75a024f
oxydy <dwhongwei@gmail.com>
2023-04-05 16:07:43 +0800

    revert commit

--- app.js
+++ app.js
@@ -14,60 +14,44 @@ const app = new App({
     console.log('⚡️ Bolt app is running');
 })();
 
-app.event('app_home_opened', async ({ event, say, client, view }) => {
-    console.log('⚡️Hello! Someone just opened the app to DM so we will send them a message!')
-    say(`Hello world and <@${event.user}>! `)
-
-    try {
-        /* view.publish is the method that your app uses to push a view to the Home tab */
-        await client.views.publish({
-     
-          /* the user that opened your app's app home */
-          user_id: event.user,
-     
-          /* the view object that appears in the app home*/
-          view: {
-            type: 'home',
-            callback_id: 'home_view',
-     
-            /* body of the view */
-            blocks: [
-              {
-                "type": "section",
-                "text": {
-                  "type": "mrkdwn",
-                  "text": "*Welcome to your _App's Home_* :tada:"
-                }
-              },
-              {
-                "type": "divider"
-              },
-              {
-                "type": "section",
-                "text": {
-                  "type": "mrkdwn",
-                  "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-                }
-              },
-              {
-                "type": "actions",
-                "elements": [
-                  {
-                    "type": "button",
-                    "text": {
-                      "type": "plain_text",
-                      "text": "Click me!",
-                    }
-                  }
-                ]
-              }
-            ]
-          }
+app.shortcut('who_am_i', async ({
+    shortcut,
+    ack,
+    client
+}) => {
+    try{
+        const connection = new jsforce.Connection({
+            loginUrl: process.env.SF_LOGIN_URL
         });
-      }
-      
-      catch (error) {
+        const userInfo = await connection.login(
+            process.env.SF_USERNAME,
+            process.env.SF_PASSWD
+        );
+        console.log(userInfo);
+        await ack();
+        const result = await client.views.open({
+            trigger_id: shortcut.trigger_id,
+            view: {
+                type: "modal",
+                title: {
+                    type: "plain_text",
+                    text: "My App"
+                },
+                close: {
+                    type: "plain_text",
+                    text: "Close"
+                },
+                blocks: [{
+                    type: "section",
+                    text: {
+                        type: "mrkdwn",
+                        text: "Logged in with user using userId ${userInfo.id}"
+                    }
+                }]
+            }
+        });
+        console.log(result);
+    }catch(error){
         console.error(error);
-      }
-
+    }
 });
